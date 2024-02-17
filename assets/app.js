a174b8edf64fd43180810bdb184a1e0d808296cf
AgathaBssn <agatha.basson@gmail.com>
2024-02-17 17:21:20 +0100

    gérer day null

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,35 @@
+
+function checkForm(){
+    //getDate
+    let day = document.getElementById('day').value;
+    let month = document.getElementById('month').value;
+    let year = document.getElementById('year').value;
+    let dateInput = new Date(year, month, day);
+    
+    //Test if inputs filled
+    if (day === '' || month === '' || year === ''){
+        let errorMssg = 'This field is required';
+        if (day === ''){
+            document.getElementById('day').style.borderColor = '#ff4b4b';
+            let thisError = document.getElementById('nameErrorDay');
+            document.getElementById('nameErrorDay').textContent = errorMssg;
+            thisError.style.display = 'block';
+        }
+        if (month === '') {
+            document.getElementById('month').style.borderColor = '#ff4b4b';
+            let thisError = document.getElementById('nameErrorMonth');
+            document.getElementById('nameErrorMonth').value = errorMssg;
+            thisError.style.display = 'block';
+        }
+        if (year === '') {
+            document.getElementById('year').style.borderColor = '#ff4b4b';
+            let thisError = document.getElementById('nameErrorYear');
+            document.getElementById('nameErrorYear').value = errorMssg;
+            thisError.style.display = 'block';
+        }
+    }
+    
+}
 function calculator() {
     //date de naissance en input
     let day = document.getElementById('day').value;
@@ -11,7 +43,6 @@ function calculator() {
     let todayYear = today.getFullYear();
     
     let diff = today.getTime() - new Date(year, month, day).getTime();
-    
     
         //date à renvoyer en prennant en compte les années bissextiles
     let yearDiff = todayYear - year;
@@ -29,6 +60,4 @@ function calculator() {
     document.getElementById('yearP').innerHTML = yearDiff;
     document.getElementById('monthP').innerHTML = monthDiff;
     document.getElementById('dayP').innerHTML = dayDiff;
-    
-    
 }
