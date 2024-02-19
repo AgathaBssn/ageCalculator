b7b1984aacf1b61027732e4abf1c1f0c2c234bda
AgathaBssn <agatha.basson@gmail.com>
2024-02-19 15:10:35 +0100

    filtrer date format

--- assets/app.js
+++ assets/app.js
@@ -5,29 +5,47 @@ function checkForm(){
     let month = document.getElementById('month').value;
     let year = document.getElementById('year').value;
     let dateInput = new Date(year, month, day);
-    
+    //convert in red
+    function convertInRed(day){
+        document.getElementById(day).style.borderColor = '#ff4b4b';
+        document.getElementById('input-' + day).style.color = '#ff4b4b';
+        let thisError = document.getElementById('nameError' + day.charAt(0).toUpperCase() + day.slice(1));
+        thisError.style.display = 'block';
+    }
     //Test if inputs filled
     if (day === '' || month === '' || year === ''){
         let errorMssg = 'This field is required';
         if (day === ''){
-            document.getElementById('day').style.borderColor = '#ff4b4b';
-            let thisError = document.getElementById('nameErrorDay');
+            convertInRed('day');
             document.getElementById('nameErrorDay').textContent = errorMssg;
-            thisError.style.display = 'block';
         }
         if (month === '') {
-            document.getElementById('month').style.borderColor = '#ff4b4b';
-            let thisError = document.getElementById('nameErrorMonth');
+            convertInRed('month');
             document.getElementById('nameErrorMonth').textContent = errorMssg;
-            thisError.style.display = 'block';
         }
         if (year === '') {
-            document.getElementById('year').style.borderColor = '#ff4b4b';
-            let thisError = document.getElementById('nameErrorYear');
+            convertInRed('year');
             document.getElementById('nameErrorYear').textContent = errorMssg;
-            thisError.style.display = 'block';
         }
     }
+    //valid format
+    if ((day >= 32 || day <= 0) && day !== ''){
+        let errorMssg = 'Must be a valid day';
+        convertInRed('day');
+        document.getElementById('nameErrorDay').textContent = errorMssg;
+    } 
+    if ((month >= 13 || month <= 0) && month !== ''){
+        let errorMssg = 'Must be a valid month';
+        convertInRed('month');
+        document.getElementById('nameErrorMonth').textContent = errorMssg;
+    }
+    let thisYear = new Date().getFullYear();
+    if (year > thisYear){
+        let errorMssg = 'Must be in the past';
+        convertInRed('year');
+        document.getElementById('nameErrorYear').textContent = errorMssg;
+    }
+        
     
 }
 function calculator() {
